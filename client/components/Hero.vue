67e9a484c98e4f8fd21bd027e6792e4058a7f56b
Chrystian Ferreira <chrystian.profissional@gmail.com>
2023-02-10 17:54:44 -0300

    First components and pages created using Strapi
    This commit is a work in progress. It brings 'Footer' and 'Menu' single types and 'page' collection type that contains 'Hero', 'Link', 'SocialLink' and 'TextBlock' components. It also brings the very start of front-end integration using @nuxtjs/strapi library @ 1.7.1

---
+++ client/components/Hero.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h1>{{ hero.heading }}</h1>
+
+    <p>{{ hero.description }}</p>
+  </section>
+
+</template>
+
+<script setup>
+defineProps({
+  hero: {
+    type: Object,
+    default: () => {
+      return {};
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,9 +1,25 @@
 <template>
 	<h1>Home</h1>
+
+	<Hero :data="hero" />
 </template>
 
-<script>
 
+<script setup>
+	const hero = reactive({});
+	
+	const getPageData = async () => {
+		const { find } = useStrapi4()
+		try {
+			const { data } = await find('pages/1?populate=*');
+
+			hero.value = { ...data.attributes.hero };
+		} catch (e) {
+			console.error(e);
+		}
+	}
+
+	getPageData();
 </script>
 
 <style lang="scss" scoped>
